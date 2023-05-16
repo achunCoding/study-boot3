2f5d7fa8a0d49d5c631059fa118d136998046069
‘wangyanchun’ <wycfight@163.com>
2023-05-16 17:59:18 +0800

    feat: springboot3 first commit add maven config and gateway

---
+++ boot3-gateway/src/main/java/cn/wycfight/study/Boot3GatewayApplication.java
@@ -0,0 +1,15 @@
+package cn.wycfight.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Boot3GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot3GatewayApplication.class, args);
+	}
+
+}
