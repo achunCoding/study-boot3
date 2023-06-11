320714a318e8e4a34c462818622b8b11aad1ff5e
‘wangyanchun’ <wycfight@163.com>
2023-06-11 09:10:22 +0800

    feat: springboot3 add oauth2.0 demo -> authorizationserver

---
+++ boot3-oauth2-example/boot3-authorizationserver/src/main/java/cn/wycfight/cloud/authorization/AuthorizationServerApplication.java
@@ -0,0 +1,14 @@
+package cn.wycfight.cloud.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author achun
+ */
+@SpringBootApplication
+public class AuthorizationServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
+}
