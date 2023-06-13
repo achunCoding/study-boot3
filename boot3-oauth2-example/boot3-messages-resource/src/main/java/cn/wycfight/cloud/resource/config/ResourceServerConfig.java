853da8babf7b0ecd2a7e796b4fb1a7241ec68d55
‘wangyanchun’ <wycfight@163.com>
2023-06-13 18:08:50 +0800

    feat: springboot3 add oauth2.0 demo -> resource server  and client server

--- boot3-oauth2-example/boot3-messages-resource/src/main/java/cn/wycfight/cloud/resource/config/ResourceServerConfig.java
+++ boot3-oauth2-example/boot3-messages-resource/src/main/java/cn/wycfight/cloud/resource/config/ResourceServerConfig.java
@@ -1,9 +1,11 @@
 package cn.wycfight.cloud.resource.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -11,17 +13,22 @@ import org.springframework.security.web.SecurityFilterChain;
 public class ResourceServerConfig {
 
 
+    /**
+     * 配置Spring Security Filter 过滤链  http.securityMatcher 表示只有/messages/** 请求匹配才会进入此过滤连
+     * authorizeHttpRequests 表示只有message.read权限才能访问 仅对/messages/**下路径有效
+     * oauth2ResourceServer 配置Oauth2资源服务器，使用JWT令牌进行身份认证和授权
+     * http.build() 方法将创建一个 SecurityFilterChain Bean，并返回它 以保护WEB资源
+     * @param http
+     * @return
+     * @throws Exception
+     */
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.securityMatcher("/messages/**")
-                .authorizeHttpRequests()
-                .requestMatchers("/messages/**")
-                .hasAuthority("SCOPE_message.read")
-                .and()
-                .oauth2ResourceServer()
-                .jwt();
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.
+                        requestMatchers("/messages/**").hasAuthority("SCOPE_message.read"))
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return http.build();
-
     }
 
 
